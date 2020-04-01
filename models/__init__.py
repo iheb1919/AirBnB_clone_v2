6c6c948d195c45675cbf7d773c4df8dbae758d60
iheb1919 <ihebmejri14@gmail.com>
2020-04-02 00:47:31 +0100

    pep8

--- models/__init__.py
+++ models/__init__.py
@@ -13,10 +13,10 @@ import os
 
 storage_type = os.getenv("HBNB_TYPE_STORAGE")
 if storage_type == "db":
-	from models.engine.db_storage import DBStorage
-	storage = DBStorage()
+        from models.engine.db_storage import DBStorage
+        storage = DBStorage()
         storage.reload()
 else:
-	from models.engine.file_storage import FileStorage
-	storage = FileStorage()
+        from models.engine.file_storage import FileStorage
+        storage = FileStorage()
         storage.reload()
