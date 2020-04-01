0cb391d5536f88714deb8bb95bbf62dcc2559612
iheb1919 <ihebmejri14@gmail.com>
2020-04-02 00:39:18 +0100

    ff

--- models/state.py
+++ models/state.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
 """This is the state class"""
 from models.base_model import BaseModel
-
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+import models
 
 class State(BaseModel):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+     __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="save-update, delete")
+    @property
+    def cities(self):
+        """ Getter attribute that returns a dictionary of cities in a state """
+        citty = models.storage.all(City)
+        dictt = {}
+        for key, value in citty.items():
+            if value.id == self.id:
+                dictt[key] = value
+        return dictt
