4a430123e27fa22fd65defb22e040f79f6c1f715
FaroukAjimi <benajimi@gmail.com>
2020-04-01 17:34:44 +0100

    user

--- models/user.py
+++ models/user.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import BaseModel
-from sqlalchemy import create_engine, Column, Integer, String
 
-class User(BaseModel):
+from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+
+class User(BaseModel, Base):
     """This is the class for user
     Attributes:
         email: email address
@@ -14,5 +16,5 @@ class User(BaseModel):
     __tablename__ = "Users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
