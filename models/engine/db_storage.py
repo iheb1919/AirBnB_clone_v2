cc09d4216e08c12aef3243badd2076af7a218a7c
iheb1919 <ihebmejri14@gmail.com>
2020-04-02 18:18:03 +0100

    ff

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -75,6 +75,6 @@ class DBStorage():
         """
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(expire_on_commit=False)
-        Session.configure(bind=self.__engine)
         Session = scoped_session(session_factory)
+        Session.configure(bind=self.__engine)
         self.__session = Session()
