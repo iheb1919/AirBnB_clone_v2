0a342d7888dcc48e0cba4c22c5cdd9f51a5d210e
iheb1919 <ihebmejri14@gmail.com>
2020-04-02 18:30:11 +0100

    fix

--- models/place.py
+++ models/place.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import create_engine, Column, Integer, Float, String,ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel, Base):
