9c2bdb0335a3fc6fc6c83f047c27fbc3d42950d7
iheb1919 <ihebmejri14@gmail.com>
2020-04-02 17:05:53 +0100

    ff

--- models/place.py
+++ models/place.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+
 class Place(BaseModel, Base):
     """This is the class for Place
     Attributes:
