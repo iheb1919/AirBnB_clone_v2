4e0ee8430e15ba2920027556e1195122d60978f8
iheb1919 <ihebmejri14@gmail.com>
2020-06-30 16:42:49 +0100

    try3

--- models/state.py
+++ models/state.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """This is the state class"""
-
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
+from os import environ
 import models
-from models.city import City
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
 
 class State(BaseModel, Base):
@@ -13,19 +13,19 @@ class State(BaseModel, Base):
     Attributes:
         name: input name
     """
-
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",
+    if 'HBNB_TYPE_STORAGE' in environ and environ['HBNB_TYPE_STORAGE'] == 'db':
+         cities = relationship("City", backref="state",
                           cascade="save-update, delete")
-
-    @property
-    def cities(self):
-        """
-        """
-        citty = models.storage.all(City)
-        dictt = {}
-        for k, v in citty.items():
-            if v.id == self.id:
-                dictt[k] = va
-        return dictt
+    else:
+        @property
+        def cities(self):
+            """
+            """
+            citty = models.storage.all(City)
+            dictt = {}
+            for k, v in citty.items():
+                if v.id == self.id:
+                    dictt[k] = va
+            return dictt
